7825b3b22af97a6c0e105dd2f4dae62526196d34
Ian Mitchell <ian.mitchel1@live.com>
2017-02-27 14:54:48 -0800

    Update actor.test.js

--- actor.test.js
+++ actor.test.js
@@ -2,12 +2,14 @@ import Actor from './actor';
 import StaticPerson from './static-person';
 
 describe('Actor', () => {
+  // Passes
   test('returns default name', () => {
     const actor = new Actor();
     StaticPerson.getName = jest.fn().mockReturnValueOnce('David');
     expect(actor.getDefaultName()).toBe('David');
   });
 
+  // Fails (because of the mock from above)
   test('sets new game', () => {
     const actor = new Actor();
     expect(actor.setNewName('Vanessa')).toBe('Vanessa');
